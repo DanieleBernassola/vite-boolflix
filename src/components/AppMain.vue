b86abd769e74069e26397c7686de8f207933d1cf
DanieleBernassola <danielebernassola02@gmail.com>
2024-06-07 17:01:49 +0200

    [UPD] Modified number rating into stars [milestone 3]

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,30 +21,46 @@ export default {
     <!-- LISTA PER FILM -->
     <li v-for="(movie, i) in this.store.apiInfo.movies">
       <p class="mb-1">Film numero {{ i }}:</p>
+      <!-- IMMAGINE FILM -->
       <img :src="this.store.apiInfo.imgUrl + this.store.apiInfo.endpoints.width + movie.poster_path" alt="movie">
+      <!-- TITOLI -->
       <p>{{ movie.title }}</p>
       <p>{{ movie.original_title }}</p>
+      <!-- LINGUA -->
       <img v-if="movie.original_language === 'it'" src="/img/italy.jpeg" alt="italy" class="language"></img>
       <img v-else-if="movie.original_language === 'en'" src="/img/england.jpg" alt="england" class="language"></img>
       <p v-else>{{ movie.original_language }}</p>
-      <p class="mb-1">{{ roundVoteAverage(movie.vote_average) }}</p>
-      <font-awesome-icon icon="fa-solid fa-star"></font-awesome-icon>
-      <font-awesome-icon icon="fa-regular fa-star"></font-awesome-icon>
+      <!-- VOTO -->
+      <p v-if="roundVoteAverage(movie.vote_average) === 0">Movie not rated</p>
+      <div v-else>
+        <font-awesome-icon v-for="icon in roundVoteAverage(movie.vote_average)"
+          icon="fa-solid fa-star"></font-awesome-icon>
+        <font-awesome-icon v-for="icon in 5 - roundVoteAverage(movie.vote_average)"
+          icon="fa-regular fa-star"></font-awesome-icon>
+      </div>
     </li>
   </ul>
   <!-- LISTA PER SERIE -->
   <ul>
     <li v-for="(serie, i) in this.store.apiInfo.series">
       <p class="mb-1">Serie numero {{ i }}:</p>
+      <!-- IMMAGINE SERIE -->
       <img :src="this.store.apiInfo.imgUrl + this.store.apiInfo.endpoints.width + serie.poster_path" alt="serie">
+      <!-- NOME SERIE -->
       <p>{{ serie.name }}</p>
       <p>{{ serie.original_name }}</p>
+      <!-- LINGUA -->
       <img v-if="serie.original_language === 'it'" src="/img/italy.jpeg" alt="italy" class="language"></img>
       <img v-else-if="serie.original_language === 'en'" src="/img/england.jpg" alt="england" class="language"></img>
       <p v-else>{{ serie.original_language }}</p>
-      <p class="mb-1">{{ roundVoteAverage(serie.vote_average) }}</p>
-      <i class="fa-solid fa-star"></i>
-      <i class="fa-regular fa-star"></i>
+      <!-- VOTO -->
+      <p v-if="roundVoteAverage(serie.vote_average) === 0">Serie not rated</p>
+      <div v-else>
+        <font-awesome-icon v-for="icon in roundVoteAverage(serie.vote_average)"
+          icon="fa-solid fa-star"></font-awesome-icon>
+        <font-awesome-icon v-for="icon in 5 - roundVoteAverage(serie.vote_average)"
+          icon="fa-regular fa-star"></font-awesome-icon>
+      </div>
     </li>
   </ul>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,15 @@ import { createApp } from "vue";
 import "./assets/scss/main.scss";
 // import vue root component
 import App from "./App.vue";
+// import libreria fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import font awesome
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import stella solid
+import { faStar as faStarSolid } from "@fortawesome/free-solid-svg-icons";
+library.add(faStarSolid);
+// import stella regular
+import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
+library.add(faStarRegular);
 
-createApp(App).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
