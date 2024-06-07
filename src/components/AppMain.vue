8cbe7b0ad92537daaec156f44092b20dace8cb98
DanieleBernassola <danielebernassola02@gmail.com>
2024-06-07 12:18:55 +0200

    [UPD] Added cover images for movies and series

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,10 +14,11 @@ export default {
     <!-- LISTA PER FILM -->
     <li v-for="(movie, i) in this.store.apiInfo.movies">
       <p class="mb-1">Film numero {{ i }}:</p>
+      <img :src="this.store.apiInfo.imgUrl + this.store.apiInfo.endpoints.width + movie.poster_path" alt="movie">
       <p>{{ movie.title }}</p>
       <p>{{ movie.original_title }}</p>
-      <img v-if="movie.original_language === 'it'" src="/img/italy.jpeg"></img>
-      <img v-else-if="movie.original_language === 'en'" src="/img/england.jpg"></img>
+      <img v-if="movie.original_language === 'it'" src="/img/italy.jpeg" alt="italy" class="language"></img>
+      <img v-else-if="movie.original_language === 'en'" src="/img/england.jpg" alt="england" class="language"></img>
       <p v-else>{{ movie.original_language }}</p>
       <p class="mb-1">{{ movie.vote_average }}</p>
     </li>
@@ -26,10 +27,11 @@ export default {
   <ul>
     <li v-for="(serie, i) in this.store.apiInfo.series">
       <p class="mb-1">Serie numero {{ i }}:</p>
+      <img :src="this.store.apiInfo.imgUrl + this.store.apiInfo.endpoints.width + serie.poster_path" alt="serie">
       <p>{{ serie.name }}</p>
       <p>{{ serie.original_name }}</p>
-      <img v-if="serie.original_language === 'it'" src="/img/italy.jpeg"></img>
-      <img v-else-if="serie.original_language === 'en'" src="/img/england.jpg"></img>
+      <img v-if="serie.original_language === 'it'" src="/img/italy.jpeg" alt="italy" class="language"></img>
+      <img v-else-if="serie.original_language === 'en'" src="/img/england.jpg" alt="england" class="language"></img>
       <p v-else>{{ serie.original_language }}</p>
       <p class="mb-1">{{ serie.vote_average }}</p>
     </li>
@@ -40,7 +42,7 @@ export default {
   margin-bottom: 1rem;
 }
 
-img {
+.language {
   width: 1rem;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import { reactive } from "vue";
 export const store = reactive({
   apiInfo: {
     apiUrl: "https://api.themoviedb.org/3",
+    imgUrl: "https://image.tmdb.org/t/p",
     apiKey: "f97905b57fea0e97859cebdf491d7a7a",
     searchQuery: "",
     movies: [],
@@ -11,6 +12,7 @@ export const store = reactive({
     endpoints: {
       movies: "/search/movie",
       tvs: "/search/tv",
+      width: "/w185",
       genre: "/genre/movie/list",
     },
   },
